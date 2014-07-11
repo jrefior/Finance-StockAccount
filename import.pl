7e68d1934a812f31ec51bf89db98e61120ba5924
jrefior <jrefior@gmail.com>
2014-07-11 19:15:36 +0000

    Getting ready to split up files into different modules.

--- import.pl
+++ import.pl
@@ -16,16 +16,48 @@ sub new {
         quantity    => $init->{quantity},
         price       => $init->{price},
     };
-    return bless($self, $class);
+    bless($self, $class);
+    $init and $self->set($init);
+    return $self;
 }
 
-package main;
+sub set {
+    my ($self, $init) = @_;
+    for my $key (keys %init) {
+        if (exists($self->{$key})) {
+            $self->{$key} = $init->{$key};
+        }
+        else {
+            warn "Tried to set $key, but that's not a known key.\n";
+        }
+    }
+    return 1;
+}
+    
+package OptionsXPress;
+use Exporter 'import';
+@EXPORT_OK = qw(new);
 import StockTransaction;
 
 #### Expected Fields, tab separated:
 # Trade Date	Indicator	Action	Symbol/Desc	Qty	Price	Commission	Net Amount	Gain Loss	
 # 0             1           2       3           4   5       6           7           8
 my @pattern = qw(date 0 action 2 symbol 3 quantity 4 price 5);
+
+sub new {
+    my ($class, $file) = @_;
+    my $self = {
+        file                => $file,
+        fh                  => undef,
+    };
+    bless($self, $class);
+    $self->init();
+    return $self;
+}
+
+
+package main;
+import OptionsXPress;
 
 my $file = 'account_analysis.xls';
 
