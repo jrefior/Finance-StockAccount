67f0b8eecc08fe083cb78f5b1268458c917a2be6
jrefior <jrefior@gmail.com>
2014-07-11 15:41:40 +0000

    Starting some object-oriented pattern for a stock transaction.  Thinking of making this into a module I can share.

--- import.pl
+++ import.pl
@@ -1,9 +1,31 @@
 #!/usr/bin/perl
 
+package StockTransaction;
+use Exporter 'import';
+@EXPORT_OK = qw(new);
+
 use strict;
 use warnings;
 
-use Text::CSV;
+sub new {
+    my ($class, $init) = @_;
+    my $self = {
+        date        => $init->{date},
+        action      => $init->{action},
+        symbol      => $init->{symbol},
+        quantity    => $init->{quantity},
+        price       => $init->{price},
+    };
+    return bless($self, $class);
+}
+
+package main;
+import StockTransaction;
+
+#### Expected Fields, tab separated:
+# Trade Date	Indicator	Action	Symbol/Desc	Qty	Price	Commission	Net Amount	Gain Loss	
+# 0             1           2       3           4   5       6           7           8
+my @pattern = qw(date 0 action 2 symbol 3 quantity 4 price 5);
 
 my $file = 'account_analysis.xls';
 
@@ -20,15 +42,10 @@ while (my $line = <$fh>) {
     my @row = split("\t", $line);
     pop(@row);
 
-
-    my $row = {};
-    for (my $x=0; $x<scalar(@headers); $x++) {
-        if (exists($row[$x])) {
-            $row->{$headers[$x]} = $row[$x];
+    for (my $x=0; $x<scalar(@pattern)-1; $x+=2) {
+        if (exists($row[$pattern[$x+1]])) {
         }
     }
-
-    print $row->{'Symbol/Desc'}, "\n";
 }
 
 
